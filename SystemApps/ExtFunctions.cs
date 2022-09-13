ec101ad271ba754ce7a38f70fc2d95100d68e6b6
dovgoluckij_i@itstep.academy <dovgoluckij_i@itstep.academy>
2022-09-13 20:14:59 +0300

    Добавьте файлы проекта.

---
+++ SystemApps/ExtFunctions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemApps
+{
+    public class ExtFunctions
+    {
+        // dialog types
+        public const int MB_OK = 0;
+        public const int MB_YESNO = 0x00000004;
+
+        // dialog icons
+        public const int MB_ICONSTOP = 0x00000010;
+        public const int MB_ICONQUESTION = 0x00000020;
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern int MessageBox(IntPtr hWnd, String text, String caption, uint type);
+
+        public const int SW_HIDE = 0;
+        public const int SW_SHOWNORMAL = 1;
+        public const int SW_SHOWMINIMIZED = 2;
+        public const int SW_SHOWMAXIMIZED = 3;
+        public const int SW_SHOW = 5;
+
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+        public static extern IntPtr FindWindowByCaption(IntPtr ZeroOnly, string lpWindowName);
+    }
+}
---
+++ SystemApps/Program1_1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemApps
+{
+    class Program1_1
+    {
+        static void Main(string[] args)
+        {
+            int res = ExtFunctions.MessageBox(IntPtr.Zero, "Hello user!!!", "Test message", 0);
+            Console.WriteLine("You press: " + res.ToString());
+            res = ExtFunctions.MessageBox(IntPtr.Zero, "Hello user!!!", "Test question", ExtFunctions.MB_YESNO | ExtFunctions.MB_ICONQUESTION);
+            Console.WriteLine("You press: " + res.ToString());
+
+            Console.Write("\nTest hide window title = ");
+            string title = Console.ReadLine();
+            IntPtr hwnd = ExtFunctions.FindWindowByCaption(IntPtr.Zero, title);
+            if ((hwnd == IntPtr.Zero) || (hwnd == null))
+            {
+                Console.WriteLine("Window not found");
+            }
+            else
+            {
+                ExtFunctions.ShowWindow(hwnd, ExtFunctions.SW_HIDE);
+                Console.WriteLine("Press key for show window");
+                Console.ReadKey();
+                ExtFunctions.ShowWindow(hwnd, ExtFunctions.SW_SHOWMAXIMIZED);
+                Console.WriteLine("Press key for show window");
+                Console.ReadKey();
+                ExtFunctions.ShowWindow(hwnd, ExtFunctions.SW_SHOWMINIMIZED);
+                Console.WriteLine("Press key for show window");
+                Console.ReadKey();
+                ExtFunctions.ShowWindow(hwnd, ExtFunctions.SW_SHOWNORMAL);
+                Console.WriteLine("Press key for show window");
+                Console.ReadKey();
+                ExtFunctions.ShowWindow(hwnd, ExtFunctions.SW_SHOW);
+            }
+
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+    }
+}
