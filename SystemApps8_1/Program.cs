d84530e53f5099fb551d8f3a18d7751d9eb02d03
dovgoluckij_i@itstep.academy <dovgoluckij_i@itstep.academy>
2022-09-28 21:13:44 +0300

    Lesson 8

---
+++ SystemApps8_1/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace SystemApps8_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test1ParallelInvoke();
+            //Test2ParallelFor();
+            //Test3ParallelForEachToken();
+            Test4ParallelAsParallel();
+
+            Console.WriteLine("\npress any key...");
+            Console.ReadKey();
+        }
+
+        static void Test1ParallelInvoke()
+        {
+            Parallel.Invoke(
+                () => 
+                { 
+                    Thread.Sleep(1500); 
+                    Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} >> Hello"); 
+                },
+                () =>
+                {
+                    Thread.Sleep(1500); 
+                    Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} >> Date {DateTime.Now.ToShortDateString()}");
+                },
+                () =>
+                {
+                    Thread.Sleep(1500); 
+                    Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} >> Time {DateTime.Now.ToLongTimeString()}");
+                },
+                Calculate
+                );
+        }
+
+        static void Calculate()
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} >> Calculate()");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+
+        static void Test2ParallelFor()
+        {
+            List<int> nums = new List<int>() { 1, 2, 3, 7, 8, 6, 8, 9 };
+            ParallelLoopResult result = Parallel.For(0, nums.Count,
+                i =>
+                {
+                    Thread.Sleep(500);
+                    Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} >> {i} ^ 2 = {i * i}");
+                }
+                );
+
+            ParallelLoopResult result1 = Parallel.ForEach(nums, Factorial);
+            if (result1.IsCompleted)
+            {
+                Console.WriteLine("Parallel.ForEach Completed");
+            }
+            else
+            {
+                Console.WriteLine("Parallel.ForEach break on " + result1.LowestBreakIteration.ToString());
+            }
+        }
+
+        static object lc = new object();
+        static void Factorial(int n, ParallelLoopState pls)
+        {
+            Thread.Sleep(1000);
+            lock (lc)
+            {
+                if (n == 0) pls.Break();
+                //if (n == 3) pls.Break();
+                //if (n == 7) pls.Stop();
+
+                int f = 1;
+                for (int i = 1; i <= n; i++)
+                {
+                    f *= i;
+                }
+                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} >> {n}! = {f}");
+            }
+        }
+
+        static void Test3ParallelForEachToken()
+        {
+            List<int> nums = new List<int>() { 1, 2, 3, 7, 8, 6, 8, 9 };
+
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken token = cancellationTokenSource.Token;
+
+            Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(1000);
+                cancellationTokenSource.Cancel();
+            });
+
+            try
+            {
+                Parallel.ForEach(nums, new ParallelOptions() { CancellationToken = token }, Factorial);
+            }
+            catch(OperationCanceledException ex)
+            {
+                Console.WriteLine("Cancel!!! " + ex.Message);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("ERROR! " + ex.Message);
+            }
+            finally
+            {
+                cancellationTokenSource.Dispose();
+            }
+        }
+
+        static Random random = new Random();
+        static void Test4ParallelAsParallel()
+        {
+            List<int> nums = new List<int>();
+            for (int i = 0; i < 20; i++)
+            {
+                nums.Add(random.Next(10, 1000));
+            }
+
+            //var result = nums.AsParallel().Select(n => n * n);
+            //result.ForAll(n => Console.WriteLine($"{n} "));
+
+            //var result = from n in nums.AsParallel()
+            //             select (n * n);
+
+            (from n in nums.AsParallel()
+             select (n * n)).ForAll(n => Console.WriteLine($"{n} "));
+
+
+            //Parallel.ForEach(result, n => Console.WriteLine($"{n} "));
+        }
+    }
+}
---
+++ SystemApps8_2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace SystemApps8_2
+{
+    class Program
+    {
+        static IntPtr hook = IntPtr.Zero;
+        static IntPtr hookMs = IntPtr.Zero;
+        static void Main(string[] args)
+        {
+            hook = SetHook(HookCallback, WH_KEYBOARD_LL);
+            hookMs = SetHook(HookCallbackMS, WH_MOUSE_LL);
+            Application.Run();
+            UnhookWindowsHookEx(hook);
+            UnhookWindowsHookEx(hookMs);
+        }
+
+        static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if ((nCode >= 0) && (wParam == (IntPtr)WM_KEYDOWN))
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                if (((Keys)vkCode == Keys.LWin) || ((Keys)vkCode == Keys.RWin))
+                {
+                    Console.WriteLine("{0} blocked!", (Keys)vkCode);
+                    return (IntPtr)1;
+                }
+            }
+            return CallNextHookEx(hook, nCode, wParam, lParam);
+        }
+
+        static IntPtr HookCallbackMS(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if ((nCode >= 0) && (wParam == (IntPtr)WM_MOUSEMOVE))
+            {
+                int X = Marshal.ReadInt32(lParam);
+                if (X < 400) return (IntPtr)1;
+            }
+            return CallNextHookEx(hookMs, nCode, wParam, lParam);
+        }
+
+        private static IntPtr SetHook(HookProc proc, int whMessage)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(whMessage, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        const int WM_KEYDOWN = 0x0100;
+        const int WH_KEYBOARD_LL = 13;
+        const int WH_MOUSE_LL = 14;
+        const int WM_MOUSEMOVE = 0x0200;
+
+        public delegate IntPtr HookProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern IntPtr SetWindowsHookEx(int idHook, HookProc hookProc, IntPtr hMod, uint threadId);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern IntPtr CallNextHookEx(IntPtr hHook, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern bool UnhookWindowsHookEx(IntPtr hHook);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+    }
+}
