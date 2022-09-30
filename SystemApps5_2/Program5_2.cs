2cc90f1e18b8ebb6e91c8dc3fb0572a01a5bdddd
dovgoluckij_i@itstep.academy <dovgoluckij_i@itstep.academy>
2022-09-30 16:59:11 +0300

    mutex changes

--- SystemApps5_2/Program5_2.cs
+++ SystemApps5_2/Program5_2.cs
@@ -13,20 +13,24 @@ namespace SystemApps5_2
     {
         static Random random = new Random();
         const string mutexName = "Summ";
+        const string mutexName2 = "Summ2";
         static void Main(string[] args)
         {
             int[] nums = new int[20];
             Mutex mutex = new Mutex(true, mutexName);
             Thread thread1 = new Thread(GenerateNums);
             Thread thread2 = new Thread(Summ);
+            Thread thread3 = new Thread(Avg);
 
             thread1.Start(nums);
             mutex.ReleaseMutex();
             thread2.Start(nums);
+            thread3.Start(nums);
 
 
             thread1.Join();
             thread2.Join();
+            thread3.Join();
             Console.WriteLine("\nPress any key...");
             Console.ReadKey();
         }
@@ -46,7 +50,6 @@ namespace SystemApps5_2
             }
             mutex.ReleaseMutex();
         }
-
         static void Summ(object nums)
         {
             Mutex mutex = Mutex.OpenExisting(mutexName);
@@ -65,6 +68,30 @@ namespace SystemApps5_2
 
             Console.WriteLine($"\nSumm = {s}");
             mutex.ReleaseMutex();
+            Mutex mutex2 = new Mutex(false, mutexName2);
+        }
+
+        static void Avg(object nums)
+        {
+            Mutex mutex2;
+            while (!Mutex.TryOpenExisting(mutexName2, out mutex2))
+            {
+                Thread.Sleep(10);
+            }
+            mutex2.WaitOne();
+            Console.WriteLine("Calculate Avg");
+            int[] a = nums as int[];
+            if (a == null)
+                throw new ArgumentNullException();
+
+            int s = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                s += a[i];
+            }
+
+            Console.WriteLine($"\nAvg = {s / a.Length}");
+            mutex2.ReleaseMutex();
         }
     }
 }
